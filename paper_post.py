b4cc552963b66cf5cd03e2c9e07194767d7b69dc
yuzuki_windows <yuzukii1004@gmail.com>
2023-05-09 18:28:38 +0900

    update paper_post.py

--- paper_post.py
+++ paper_post.py
@@ -5,77 +5,102 @@ from Bio import Entrez
 import openai
 import random
 
-#OpenAI api key
-openai.api_key = ''
-# Slack API token
-SLACK_API_TOKEN = ''
-# Slack channel name
-SLACK_CHANNEL = "#general"
-# Maximum number of paper scraping at a time
-MAX_RESULTS = 100
+
+openai.api_key = 'YourOpenAIToken'   # OpenAI API token
+SLACK_API_TOKEN = 'YourSlackToken' # Slack API token
+SLACK_CHANNEL = "#general"  # Slack channel name
+
+MAX_RESULTS = 10            # Maximum number of paper scraped at a time
+NUM_CONTENTS = 3            # Number of contents to be posted at a time (chosen randomly)
+
+EMAIL = 'your_email@example.com'
+JOURNAL_LIST = ['Nat Neurosci']
+QUERY = 'fMRI'
 
 def get_summary(result):
-    system = """与えられた論文の要点を3点のみでまとめ、以下のフォーマットで日本語で出力してください。```
-    タイトルの日本語訳
-    ・要点1
-    ・要点2
-    ・要点3
-    ```"""
+    """
+    FUNCTION: get_summary
+        This function calls chatGPT 3.5-turbo API to summarize the given paper in 
+        only three points.
+        Input: result (dict) - a dictionary containing the information of a paper
+        Output: message (str) - a string containing the summary
+    """
 
-    text = f"title: {result.title}\nbody: {result.summary}"
+    # ChatGPT input
+    system = """Summarize the given paper in only three points and output the results using the following format```
+    Paper's Title
+    ・Point 1
+    ・Point 2
+    ・Point 3
+    ```"""
+    text = f"title: {result['Title']}\nbody: {result}"
+
+    # call chatGPT API
     response = openai.ChatCompletion.create(
-                model="gpt-4",
+                model="gpt-3.5-turbo",
                 messages=[
                     {'role': 'system', 'content': system},
                     {'role': 'user', 'content': text}
                 ],
                 temperature=0.25,
             )
+    
+    # create the response message
     summary = response['choices'][0]['message']['content']
-    title_en = result.title
     title, *body = summary.split('\n')
     body = '\n'.join(body)
-    date_str = result.published.strftime("%Y-%m-%d %H:%M:%S")
-    message = f"発行日: {date_str}\n{result.entry_id}\n{title_en}\n{title}\n{body}\n"
-    
-    return message
+    pub_date = result['EPubDate']
+    url = f"https://doi.org/{result['DOI']}"
+    journal = result['FullJournalName']
+
+    return f"*{title}*\nDate: {pub_date}\n{journal}\n{url}\n{body}\n"
   
-def main(event, context):
+
+def main():
+    """
+    FUNCTION: main
+        This is the main function of this program.
+    """
+
     # initialize slack API token
     client = WebClient(token=SLACK_API_TOKEN)
-    #queryを用意、今回は、三種類のqueryを用意
-    query ='ti:%22 Deep Learning %22'
+    Entrez.email = EMAIL 
+    query = QUERY
+    search_results, summary_results = [], []
 
-    # PubMed Entrez module
-    Entrez.email = 'email@example.com'
+    # scrape the newest [MAX_RESULTS] papers from each journal
+    for journal in JOURNAL_LIST:
+        handle = Entrez.esearch(db="pubmed", term=query + f" AND {journal}[journal]", sort="date", retmax=MAX_RESULTS)
+        record = Entrez.read(handle)
+        search_results.extend(record["IdList"])
+
+    # randomly choose [NUM_CONTENTS] papers from the scraped papers
+    random.shuffle(search_results)
+    for id in search_results[:NUM_CONTENTS]:
+        handle = Entrez.esummary(db="pubmed", id=id)
+        record = Entrez.read(handle)
+        summary_results.append(record[0])
     
-    
-    
-    
-    search = arxiv.Search(
-        query=query,  # 検索クエリ（
-        max_results=100,  # 取得する論文数
-        sort_by=arxiv.SortCriterion.SubmittedDate,  # sort papersby submission data
-        sort_order=arxiv.SortOrder.Descending,  # 新しい論文から順に取得する
+    # post the summary to Slack
+    response = client.chat_postMessage(
+        channel=SLACK_CHANNEL,
+        text='Daily Post Arrived!'
     )
-    #searchの結果をリストに格納
-    result_list = []
-    for result in search.results():
-        result_list.append(result)
-    #ランダムにnum_papersの数だけ選ぶ
-    num_papers = 3
-    results = random.sample(result_list, k=num_papers)
-    
-    # 論文情報をSlackに投稿する
-    for i,result in enumerate(results):
+    for i, result in enumerate(summary_results):
         try:
-            # Slackに投稿するメッセージを組み立てる
-            message = "今日の論文です！ " + str(i+1) + "本目\n" + get_summary(result)
-            # Slackにメッセージを投稿する
+            # create summary messages
+            message = f"*Paper {i+1}*\n{get_summary(result)}\n\n"
             response = client.chat_postMessage(
                 channel=SLACK_CHANNEL,
                 text=message
             )
             print(f"Message posted: {response['ts']}")
+
         except SlackApiError as e:
             print(f"Error posting message: {e}")
+
+if __name__ == "__main__":
+    main()
+
+
+# python paper_post.py
